18e07e4fcb540a6ad86c0f453b9495856e958434
Dylan Kelley <dmk41705@gmail.com>
2025-09-19 11:43:33 -0400

    dash
    I ADDED A FLIPPINGDASH BOYS

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,19 +4,54 @@ public class PlayerController : MonoBehaviour
 {
 
     public float moveSpeed;
-    float speedX, speedY;
-    Rigidbody2D rb;
+    private Vector2 movement;
+    public Rigidbody2D rb;
+
+    private float activeMoveSpeed;
+    public float dashSpeed;
+
+    public float dashLength = .3f, dashCooldown = 1f;
+
+    private float dashCounter;
+    private float dashCoolCounter;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        activeMoveSpeed = moveSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        speedX = Input.GetAxisRaw("Horizontal") * moveSpeed;
-        speedY = Input.GetAxisRaw("Vertical") * moveSpeed;
-        rb.linearVelocity = new Vector2 (speedX, speedY);
+        movement.Normalize();
+        movement.x = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        movement.y = Input.GetAxisRaw("Vertical") * moveSpeed;
+        rb.linearVelocity = movement * activeMoveSpeed;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (dashCoolCounter <= 0 && dashCounter <= 0)
+            {
+                activeMoveSpeed = dashSpeed;
+                dashCounter = dashLength;
+            }
+        }
+
+        if (dashCounter > 0)
+        {
+            dashCounter -= Time.deltaTime;
+
+            if (dashCounter <= 0)
+            {
+                activeMoveSpeed = moveSpeed;
+                dashCoolCounter = dashCooldown;
+            }
+        }
+
+        if (dashCoolCounter > 0)
+        {
+            dashCoolCounter -= Time.deltaTime;
+        }
     }
 }
